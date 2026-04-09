4520f1a08e73a434f16691bc953e8836279ac4ef
2400030446 <sadireddiveerababu@gmail.com>
2026-04-09 19:49:50 +0530

    Auto-parse Postgres DATABASE_URL for Spring Boot

--- backend/src/main/java/com/lms/backend/BackendApplication.java
+++ backend/src/main/java/com/lms/backend/BackendApplication.java
@@ -7,6 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		String dbUrl = System.getenv("DATABASE_URL");
+		if (dbUrl != null && dbUrl.startsWith("postgres://")) {
+			try {
+				java.net.URI dbUri = new java.net.URI(dbUrl);
+				String username = dbUri.getUserInfo().split(":")[0];
+				String password = dbUri.getUserInfo().split(":")[1];
+				String dbUrlJdbc = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+				System.setProperty("spring.datasource.url", dbUrlJdbc);
+				System.setProperty("spring.datasource.username", username);
+				System.setProperty("spring.datasource.password", password);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
